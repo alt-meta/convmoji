7645302612732f44573e62be0c9046ad3d5a59f5
arrrrrmin <info@dotarmin.info>
2021-10-06 12:50:36 +0200

    ✨: meine commit message

---
+++ fluffy_waffle/__init__.py
@@ -0,0 +1,12 @@
+# | Type     | Emoji                 | code                    |
+# |:---------|:----------------------|:------------------------|
+# | feat     | :sparkles:            | `:sparkles:`            |
+# | fix      | :bug:                 | `:bug:`                 |
+# | docs     | :books:               | `:books:`               |
+# | style    | :gem:                 | `:gem:`                 |
+# | refactor | :hammer:              | `:hammer:`              |
+# | perf     | :rocket:              | `:rocket:`              |
+# | test     | :rotating_light:      | `:rotating_light:`      |
+# | build    | :package:             | `:package:`             |
+# | ci       | :construction_worker: | `:construction_worker:` |
+# | chore    | :wrench:              | `:wrench:`              |
